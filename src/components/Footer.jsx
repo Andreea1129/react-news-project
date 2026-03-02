224f4d44d51d09f89ff80f3a094a3013221ab707
Andreea1129 <vilcuandreea12@gmail.com>
2026-03-02 10:08:58 +0200

    update content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <footer className="bg-dark">
       <Container>
         <p className="text-light text-center m-0 py-3">
-          IT School News România © 2026. Toate drepturile rezervate.
+          Record News România ©2026. Toate drepturile rezervate.
         </p>
       </Container>
     </footer>
